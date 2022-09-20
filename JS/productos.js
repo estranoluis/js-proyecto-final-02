b56463057ecba943875ea8bf97c92c1c1b9ed074
Luis Estrano <estranoluis11866@gmail.com>
2022-09-20 11:21:12 -0300

    Segunda entrega Proyecto Final

---
+++ JS/productos.js
@@ -0,0 +1,11 @@
+const productos = [
+    {id:1, nombre:"Iphone 13", descripcion: "256 GB", precio: 1000000},
+    {id:2, nombre:"Samsung Galaxy S22", descripcion: "128 GB", precio: 800000},
+    {id:3, nombre:"Xiaomi 12", descripcion: "128 GB", precio: 700000},
+    {id:4, nombre:"Computador Ryzen 7", descripcion:"HP", precio: 1200000},
+    {id:5, nombre:"Laptop i7", descripcion: "Asus", precio: 2000000},
+    {id:6, nombre:"Laptop i5", descripcion: "Acer", precio: 2100000},
+    {id:7, nombre:"Audifonos", descripcion: "Hyper X", precio: 100000},
+    {id:8, nombre:"Mouse", descripcion:"Razer", precio: 80000},
+    {id:9, nombre:"Case", descripcion:"Original", precio: 30000},
+]
